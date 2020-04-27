30341a585aa673efceb361a0343e250de271d114
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2020-04-27 18:05:04 +0100

    Scoring Working (with help)

--- boardgame prototype/Assets/Scripts/contactColour.cs
+++ boardgame prototype/Assets/Scripts/contactColour.cs
@@ -1,67 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class contactColour : MonoBehaviour
 {
-    public Color Color { get; private set; }
+    [SerializeField] MeshRenderer m_meshRenderer; // Cache as GetComponent is expensive https://connect.unity.com/p/getcomponent-only-once
+    private int m_shaderColourID;
+    private PlayerID m_owningPlayer = PlayerID.None;
 
-   public float redScore = 0.0f;
-   public float greenScore = 0.0f;
-   public float blueScore = 0.0f;
-   public float yellowScore = 0.0f;
-   public float tileCount = 0.0f;
-   public bool colourRed = false;
-   public bool colourGreen = false;
-   public bool colourBlue = false;
-   public bool colourYellow = false;
-    // public Color color = Color.red;
+    public Action<PlayerID, PlayerID, float> OnScore;
+
+
+
+    private void Awake()
+    {
+        if(m_meshRenderer == null)
+        {
+            Debug.LogWarning("Assign mesh renderer in inspector.");
+            m_meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        }
+
+        // Cache shader ID for "_Color", minor optimisation https://docs.unity3d.com/ScriptReference/Shader.PropertyToID.html
+        m_shaderColourID = Shader.PropertyToID("_Color");
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
-        if(collision.gameObject.tag == "Player1") //on contact with the Red player, changes the tile to red 
+        if(collision.gameObject.tag == "Player1" && m_owningPlayer != PlayerID.Red)
         {
-            colourRed = true; //triggers the bool for scoreCounter to increase 
-            var cubeRenderer = gameObject.GetComponent<Renderer>();
-            cubeRenderer.material.SetColor("_Color", Color.red);
-            Debug.Log(redScore); //updates the score 
-        }
-       // if (colourRed == true)
-       // {
-       //     redScore = redScore + 1;
-       //     Debug.Log("Red Score is: " + redScore);
-       // }
-
-
-        if (collision.gameObject.tag == "Player2")//on contact with the Green player, will change the tile to green 
-        {
-            colourGreen = true;
-            var cubeRenderer = gameObject.GetComponent<Renderer>();
-            cubeRenderer.material.SetColor("_Color", Color.green);
-           // tileCount = tileCount + 1;
-            Debug.Log(greenScore);
-          // if (colourGreen == true)
-          // {
-          //     greenScore = greenScore + 1;
-          //     Debug.Log("Green Score is: " + greenScore);
-          // }
-        }
-        
-
-        if (collision.gameObject.tag == "Player3")//on contact with the Green player, will change the tile to blue
-        {
-            colourBlue = true;
-            var cubeRenderer = gameObject.GetComponent<Renderer>();
-            cubeRenderer.material.SetColor("_Color", Color.blue);
-            Debug.Log(blueScore);
+            m_meshRenderer.material.SetColor(m_shaderColourID, Color.red);
+            OnScore?.Invoke(PlayerID.Red, m_owningPlayer, 1.0f); // Call our delegate, this will call scoreCounter::OnAddScore since they subscribed in the awake call (contactColour.OnScore = OnAddScore)
+            m_owningPlayer = PlayerID.Red;
+            return; // Early out, to be safe and to save testing other cases
         }
 
-        if(collision.gameObject.tag == "Player4")//on contact with the Orange player, the tile will change to Orange
+        if (collision.gameObject.tag == "Player2" && m_owningPlayer != PlayerID.Blue)
         {
-            colourYellow = true;
-            var cubeRenderer = gameObject.GetComponent<Renderer>();
-            cubeRenderer.material.SetColor("_Color", Color.yellow);
-            Debug.Log(yellowScore);
+            m_meshRenderer.material.SetColor(m_shaderColourID, Color.blue);
+            OnScore?.Invoke(PlayerID.Blue, m_owningPlayer, 1.0f); // Call our delegate, this will call scoreCounter::OnAddScore since they subscribed in the awake call (contactColour.OnScore = OnAddScore)
+            m_owningPlayer = PlayerID.Blue;
+            return; // Early out, to be safe and to save testing other cases
+        }
+
+        if (collision.gameObject.tag == "Player3" && m_owningPlayer != PlayerID.Green)
+        {
+            m_meshRenderer.material.SetColor(m_shaderColourID, Color.green);
+            OnScore?.Invoke(PlayerID.Green, m_owningPlayer, 1.0f); // Call our delegate, this will call scoreCounter::OnAddScore since they subscribed in the awake call (contactColour.OnScore = OnAddScore)
+            m_owningPlayer = PlayerID.Green;
+            return; // Early out, to be safe and to save testing other cases
+        }
+
+        if (collision.gameObject.tag == "Player4" && m_owningPlayer != PlayerID.Yellow)
+        {
+            m_meshRenderer.material.SetColor(m_shaderColourID, Color.yellow);
+            OnScore?.Invoke(PlayerID.Yellow, m_owningPlayer, 1.0f); // Call our delegate, this will call scoreCounter::OnAddScore since they subscribed in the awake call (contactColour.OnScore = OnAddScore)
+            m_owningPlayer = PlayerID.Yellow;
+            return; // Early out, to be safe and to save testing other cases
         }
     }
 }
--- boardgame prototype/Assets/Scripts/scoreCounter.cs
+++ boardgame prototype/Assets/Scripts/scoreCounter.cs
@@ -3,35 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class scoreCounter : contactColour
+public class scoreCounter : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider collision)
+    [SerializeField] contactColour[] m_contactColours;
+    Dictionary<PlayerID, float> m_playerScores;
+
+    private void Awake()
     {
-        if (colourRed == true) //activates if the red player collides with a tile 
+        // Initialise the dictionary with empty values
+        m_playerScores = new Dictionary<PlayerID, float>
         {
-            redScore += 1; //increases the score by 1 -- trying to figure out a method that actually does this? 
-            //redScore++;
+            { PlayerID.Red, 0.0f },
+            { PlayerID.Blue, 0.0f },
+            { PlayerID.Green, 0.0f },
+            { PlayerID.Yellow, 0.0f }
+        };
+
+
+        if(m_contactColours == null || m_contactColours.Length == 0)
+        {
+            Debug.LogWarning("Contact colours not set in inspector.");
+            m_contactColours = FindObjectsOfType<contactColour>(); // Bad
         }
 
-        if (colourGreen == true) //activates if the green player collides with a tile
+        // Subscribe to all the callbacks
+        foreach (contactColour contactColour in m_contactColours)
         {
-            // greenScore = greenScore + 1; //neither of these work 
-            //greenScore++;
+            contactColour.OnScore = OnAddScore;
         }
-        Debug.Log(greenScore);
 
-        if (colourBlue == true) //activates if the blue player collides with a tile 
-        {
-            //blueScore = blueScore + 1; 
-            //blueScore++;
-        }
-        Debug.Log(blueScore);
-
-        if (colourYellow == true) //activates if the yellow player collides with a tile 
-        {
-            //yellowScore = yellowScore + 1;
-            //yellowScore++;
-        }
-        Debug.Log(yellowScore);
+        // We dont need reference to the other scripts anymore
+        m_contactColours = null;
     }
+
+    private void OnAddScore(PlayerID identifier, PlayerID previousOwner, float scoreToAdd)
+    {
+        m_playerScores[identifier] += scoreToAdd;
+        if(previousOwner != PlayerID.None)
+        {
+            m_playerScores[previousOwner] -= scoreToAdd;
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.color = Color.red;
+        GUILayout.Label($"{m_playerScores[PlayerID.Red]}");
+        GUI.color = Color.blue;
+        GUILayout.Label($"{m_playerScores[PlayerID.Blue]}");
+        GUI.color = Color.green;
+        GUILayout.Label($"{m_playerScores[PlayerID.Green]}");
+        GUI.color = Color.yellow;
+        GUILayout.Label($"{m_playerScores[PlayerID.Yellow]}");
+    }
+}
+
+public enum PlayerID
+{
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    None
 }
